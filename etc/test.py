93bd10f27da54423020467808baea96564eaf0bc
Clarence Leung <cleung0@gmail.com>
2011-09-19 19:51:53 -0400

    Improving sample histogram output

--- etc/test.py
+++ etc/test.py
@@ -1,6 +1,8 @@
-from random_metro import *
-from matplotlib.pylab import *
+from random_metro import rand_walk_metropolis
+from matplotlib.pylab import hist, ylabel, xlabel, show
 
-data = rand_walk_metropolis(10000)
-hist(data)
+data = rand_walk_metropolis(100000)
+hist(data, bins=30, normed=1)
+xlabel('x')
+ylabel('Frequency')
 show()
