4c25307552b3715bb27da7b6fac0e551923ea896
Clarence Leung <cleung0@gmail.com>
2011-09-22 19:04:43 -0400

    Adding acceptance rate to data histogram

--- etc/test.py
+++ etc/test.py
@@ -1,8 +1,9 @@
-from random_metro import rand_walk_metropolis
-from matplotlib.pylab import hist, ylabel, xlabel, show
+from mcmc import rand_walk_metropolis, indep_metropolis
+from matplotlib.pylab import hist, ylabel, xlabel, title, suptitle, show
 
-data = rand_walk_metropolis(100000)
+data, ratio = indep_metropolis(100000)
 hist(data, bins=30, normed=1)
 xlabel('x')
 ylabel('Frequency')
+title('Acceptance rate: %f' % ratio)
 show()
