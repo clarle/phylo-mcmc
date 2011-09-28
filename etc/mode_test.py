e2785355760e1620f7ae623ad1440a9102068ad5
Clarence Leung <cleung0@gmail.com>
2011-09-28 17:28:45 -0400

    Adding Hessian matrix to laplace test

--- etc/mode_test.py
+++ etc/mode_test.py
@@ -5,6 +5,4 @@ from scipy.optimize import fmin
 data = np.asarray((36, 13, 23, 6, 20, 12, 23, 93, 98, 91, 89, 100, 90, 95, 90, 87))
 start = np.asarray((90, 2))
 
-print cauchy_error_post(start, data)
-
 print laplace(cauchy_error_post, start, data=(data,))
