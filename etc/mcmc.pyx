e78b6d31fb5c21a3cbac818dc8d12ae45ab6cf6c
Clarence Leung <cleung0@gmail.com>
2011-10-26 06:15:33 -0400

    Minor bugs and improved labeling on graph'

--- etc/mcmc.pyx
+++ etc/mcmc.pyx
@@ -1,5 +1,6 @@
 import numpy as np
 import numdifftools as nd
+from numpy import matrix
 from numpy.linalg import inv, cholesky
 from scipy.stats import norm, t
 from scipy.optimize import fmin
@@ -87,17 +88,17 @@ def indep_metropolis(int draws):
 
 def rw_matrix_metropolis(log_post, var, scale, start, int draws, data=None):
     cdef np.ndarray[double, ndim=2] target = np.zeros([draws, start.size])
-    cdef np.ndarray[double, ndim=2] b = np.matrix.getT(np.matrix(start))
+    cdef np.ndarray[double, ndim=2] b = matrix.getT(matrix(start))
     cdef np.ndarray[double, ndim=1] rand_draw = np.random.rand(draws)
-    cdef np.ndarray[double, ndim=2] lower_tri = cholesky(np.matrix(var))
+    cdef np.ndarray[double, ndim=2] lower_tri = cholesky(matrix(var))
 
     cdef np.ndarray[double, ndim=2] bc
     cdef double cur, ratio
     cdef double can = log_post(start, data)
     cdef double accepted = 0.
   
-    for i in xrange(1, draws):
-        bc = b + (scale * np.matrix.getT(lower_tri)) * np.matrix.getT(np.matrix(np.random.normal(0., 1, start.size)))
+    for i in xrange(0, draws):
+        bc = b + (scale * matrix.getT(lower_tri)) * matrix.getT(matrix(np.random.normal(0., 1, start.size)))
         cur = log_post(bc, data)
         prob = exp(can - cur)
 
--- etc/mode_test.py
+++ etc/mode_test.py
@@ -1,8 +1,21 @@
 from mcmc import *
 import numpy as np
 from scipy.optimize import fmin
+from matplotlib.pylab import hist, ylabel, xlabel, title, suptitle, show
 
-data = np.asarray((36, 13, 23, 6, 20, 12, 23, 93, 98, 91, 89, 100, 90, 95, 90, 87))
-start = np.asarray((90, 2))
+data = np.asarray((36., 13., 23., 6., 20., 12., 23., 93., 98., 91., 89., 100., 90., 95., 90., 87.))
+start = np.asarray((10., 4.))
 
-print laplace(cauchy_error_post, start, data=(data,))
+mode, var = laplace(cauchy_error_post, start, data=(data,))
+
+print mode, var
+
+data, ratio = rw_matrix_metropolis(cauchy_error_post, var, 2., start, 1000, data)
+
+mean = np.mean(data, 0)
+std = np.std(data, 0)
+
+print data
+print ratio
+print mean
+print std
