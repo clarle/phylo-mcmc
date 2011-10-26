f3cd59516af68dfcf1965c92469e737f4595e21e
Clarence Leung <cleung0@gmail.com>
2011-10-26 13:53:37 -0400

    sequence and time merging functions

--- src/python/utils.py
+++ src/python/utils.py
@@ -1,7 +1,9 @@
 import operator
+import numpy as np
 from collections import Counter
+from random import choice, uniform
 
-def prod(terms)
+def prod(terms):
     return reduce(operator.mul, terms)
 
 def parse_data(name='infile'):
@@ -13,11 +15,34 @@ def parse_data(name='infile'):
 
     for i in range(len_sequences):
         seq = f.readline().strip('\n')
-        sequences.push(seq)
+        sequences.append(seq)
 
     return sequences, num_sequences, len_sequences
 
 def calc_nuc_sites(seq1, seq2):
     return Counter(((seq1[nuc], seq2[nuc]) for nuc in range(len(seq1))))
 
+def four_category_dist(draws, probs, alphabet):
+    a = np.random.multinomial(draws, probs)
+    b = np.empty(draws, dtype='|S1')
 
+    upper = np.cumsum(a)
+    lower = upper - a
+
+    for value in range(len(a)):
+        b[lower[value]:upper[value]] = alphabet[value]
+
+    np.random.shuffle(b)
+    return b
+
+def merge_seqs(seq1, seq2):
+    parent_seq = []
+    
+    for i in range(len(seq1)):
+        nuc = choice((seq1[i], seq2[i]))
+        parent_seq.append(nuc)
+
+    return parent_seq
+
+def merge_time(time1, time2):
+    return uniform(0, max(time1, time2))
